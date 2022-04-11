f84481f66f41f10ec7bd642b4206ddfd2a64ea26
Manish Kumar <97939632+codewithmanis@users.noreply.github.com>
2022-04-11 22:28:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">MovieList App</h1>
+    <p data-ns-test="project-description">We can search any movie in this application.</p>
+    </div>
   )
 }
 
