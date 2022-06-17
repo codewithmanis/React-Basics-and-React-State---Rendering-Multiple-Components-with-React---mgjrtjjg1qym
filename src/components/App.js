d7420a872fea10f0fe6a0377a694f2ad83d4ff73
Manish Kumar <97939632+codewithmanis@users.noreply.github.com>
2022-06-17 18:17:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const data = {
+   projectName : "MovieList App"
+   projectDescription : "We can search any movie in this application"
+  }
   return (
     <div id="main">
-    <h1 data-ns-test="project-name">MovieList App</h1>
-    <p data-ns-test="project-description">We can search any movie in this application.</p>
+    <h1 data-ns-test="project-name">{data.projectName}</h1>
+    <p data-ns-test="project-description">{data.projectDescription}</p>
     </div>
   )
 }
